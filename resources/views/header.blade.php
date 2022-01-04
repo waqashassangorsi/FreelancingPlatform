0e13ba11cdc23bf06e3f6a05b030f1229be14966
unknown <haseebse2954@gmail.com>
2022-01-04 14:51:55 +0500

    updated header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -7,6 +7,8 @@
 <title>Freelancing Platform</title>
 	@include('header.head')
 </head>
+
+@if (2  > 1)
 <body class="wt-login">
 	<!--[if lt IE 8]>
 		<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
@@ -20,19 +22,17 @@
 	<div id="wt-wrapper" class="wt-wrapper wt-haslayout">
 		<!-- Content Wrapper Start -->
 		<div class="wt-contentwrapper">
-			@if(2 >= 1)
-			<!-- Header Start  with login-->
+			<!-- Header Start -->
 			<header id="wt-header" class="wt-header wt-headervtwo wt-haslayout">
 				<div class="wt-navigationarea">
 					<div class="container-fluid">
 						<div class="row">
 							<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-								<strong class="wt-logo"><a href="{{url('/dashboard')}}"><img src="{{asset('frontend/images/logo.png')}}" alt="company logo here"></a></strong>
-								
+								<strong class="wt-logo"><a href="index.html"><img src="{{asset('frontend/images/logo.png')}}" alt="company logo here"></a></strong>
 								<form class="wt-formtheme wt-formbanner wt-formbannervtwo">
 									<fieldset>
 										<div class="form-group">
-											<input type="text" name="job" class="form-control">
+											<input type="text" name="job" class="form-control" >
 											<div class="wt-formoptions">
 												<div class="wt-dropdown">
 													<span>In: <em class="selected-search-type">Freelancers </em><i class="lnr lnr-chevron-down"></i></span>
@@ -51,12 +51,11 @@
 														<label for="wt-companies">Companies</label>
 													</span>
 												</div>
-												<a href="javascrip:void(0);" class="wt-searchbtn"><i class="lnr lnr-magnifier"></i></a>
+												<!-- <a href="javascrip:void(0);" class="wt-searchbtn"><i class="lnr lnr-magnifier"></i></a> -->
 											</div>
 										</div>
 									</fieldset>
 								</form>
-								
 								<div class="wt-rightarea">
 									<nav id="wt-nav" class="wt-nav navbar-expand-lg">
 										<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -86,26 +85,26 @@
 														<li class="menu-item-has-children page_item_has_children"><span class="wt-dropdowarrow"><i class="lnr lnr-chevron-right"></i></span>
 															<a href="javascript:void(0);">Company</a>
 															<ul class="sub-menu">
-																<li><a href="{{url('/companygrid')}}">Company Grid</a></li>
-																<li><a href="{{url('/companysingle')}}">Company Sigle</a></li>
+																<li><a href="companygrid.html">Company Grid</a></li>
+																<li><a href="companysigle.html">Company Sigle</a></li>
 															</ul>
 														</li>
 														<li>
-															<a href="{{url('/about')}}">About</a>
+															<a href="about.html">About</a>
 														</li>
 														<li>
-															<a href="{{url('/privacypolicy')}}">Privacy Policy</a>
+															<a href="privacypolicy.html">Privacy Policy</a>
 														</li>
 														<li>
 															<a href="comingsoon.html">Coming Soon</a>
 														</li>
 														<li>
-															<a href="{{url('/notavailable')}}">404page</a>
+															<a href="404page.html">404page</a>
 														</li>
 													</ul>
 												</li>
 												<li class="nav-item">
-													<a href="{{url('/howitworks')}}">How It Works</a>
+													<a href="howitworks.html">How It Works</a>
 												</li>
 												<li class="menu-item-has-children page_item_has_children">
 													<a href="javascript:void(0);">Browse Jobs</a>
@@ -201,12 +200,12 @@
 														<span>Insights</span>
 													</a>
 													<ul class="sub-menu children">
-														<li><a href="{{url('/dashboard')}}">Insights</a></li>
-														<li><a href="{{url('/insightsuser')}}">Insights User</a></li>
+														<li><a href="dashboard-insights.html">Insights</a></li>
+														<li><a href="dashboard-insightsuser.html">Insights User</a></li>
 													</ul>
 												</li>
 												<li>
-													<a href="{{url('/profile')}}">
+													<a href="dashboard-profile.html">
 														<span>My Profile</span>
 													</a>
 												</li>
@@ -215,23 +214,23 @@
 														<span>All Jobs</span>
 													</a>
 													<ul class="sub-menu">
-														<li><a href="{{url('/completejobs')}}">Completed Jobs</a></li>
-														<li><a href="{{url('/canceljobs')}}">Cancelled Jobs</a></li>
+														<li><a href="dashboard-completejobs.html">Completed Jobs</a></li>
+														<li><a href="dashboard-canceljobs.html">Cancelled Jobs</a></li>
 														<li><a href="dashboard-ongoingjob.html">Ongoing Jobs</a></li>
-														<li><a href="{{url('/ongoingsingle')}}">Ongoing Single</a></li>
+														<li><a href="dashboard-ongoingsingle.html">Ongoing Single</a></li>
 													</ul>
 												</li>
 												<li>
-													<a href="{{url('/managejobs')}}">
+													<a href="dashboard-managejobs.html">
 														<span>Manage Jobs</span>
 													</a>
 												</li>
 												<li class="wt-notificationicon menu-item-has-children">
-													<a href="{{url('/messages')}}">
+													<a href="javascript:void(0);">
 														<span>Messages</span>
 													</a>
 													<ul class="sub-menu">
-														<li><a href="{{url('/messages')}}">Messages</a></li>
+														<li><a href="dashboard-messages.html">Messages</a></li>
 														<li><a href="dashboard-messages2.html">Messages V 2</a></li>
 													</ul>
 												</li>
@@ -241,7 +240,7 @@
 													</a>
 												</li>
 												<li>
-													<a href="{{url('/invoices')}}">
+													<a href="dashboard-invocies.html">
 														<span>Invoices</span>
 													</a>
 												</li>
@@ -251,27 +250,27 @@
 													</a>
 												</li>
 												<li>
-													<a href="{{url('/packages')}}">
+													<a href="dashboard-packages.html">
 														<span>Packages</span>
 													</a>
 												</li>
 												<li>
-													<a href="{{url('/proposals')}}">
+													<a href="dashboard-proposals.html">
 														<span>Proposals</span>
 													</a>
 												</li>
 												<li>
-													<a href="{{url('/settings')}}">
+													<a href="dashboard-accountsettings.html">
 														<span>Account Settings</span>
 													</a>
 												</li>
 												<li>
-													<a href="{{url('/support')}}">
+													<a href="dashboard-helpsupport.html">
 														<span>Help &amp; Support</span>
 													</a>
 												</li>
 												<li>
-													<a href="{{url('/')}}">
+													<a href="index.html">
 														<span>Logout</span>
 													</a>
 												</li>
@@ -285,14 +284,27 @@
 				</div>
 			</header>
 			<!--Header End-->
-			@else
-			<!-- Header Start  without login-->
+@else
+<body class="">
+	<!--[if lt IE 8]>
+		<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+	<![endif]-->
+	<!-- Preloader Start -->
+	<div class="preloader-outer">
+		<div class="loader"></div>
+	</div>
+	<!-- Preloader End -->
+	<!-- Wrapper Start -->
+	<div id="wt-wrapper" class="wt-wrapper wt-haslayout">
+		<!-- Content Wrapper Start -->
+		<div class="wt-contentwrapper">
+			<!-- Header Start -->
 			<header id="wt-header" class="wt-header wt-haslayout">
 				<div class="wt-navigationarea">
 					<div class="container-fluid">
 						<div class="row">
 							<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-								<strong class="wt-logo"><a href="{{url('/')}}"><img src="{{asset('frontend/images/logo.png')}}" alt="company logo here"></a></strong>
+								<strong class="wt-logo"><a href="index.html"><img src="{{asset('frontend/images/logo.png')}}" alt="company logo here"></a></strong>
 								<div class="wt-rightarea">
 									<nav id="wt-nav" class="wt-nav navbar-expand-lg">
 										<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -300,17 +312,17 @@
 										</button>
 										<div class="collapse navbar-collapse wt-navigation" id="navbarNav">
 											<ul class="navbar-nav">
-												<li class="menu-item-has-children page_item_has_children"><span class="wt-dropdowarrow"><i class="lnr lnr-chevron-right"></i></span>
+												<li class="menu-item-has-children page_item_has_children">
 													<a href="javascript:void(0);">Main</a>
 													<ul class="sub-menu">
-														<li class="menu-item-has-children page_item_has_children wt-notificationicon"><span class="wt-dropdowarrow"><i class="lnr lnr-chevron-right"></i></span><span class="wt-dropdowarrow"><i class="lnr lnr-chevron-right"></i></span>
+														<li class="menu-item-has-children page_item_has_children wt-notificationicon"><span class="wt-dropdowarrow"><i class="lnr lnr-chevron-right"></i></span>
 															<a href="javascript:void(0);">Home</a>
 															<ul class="sub-menu">
 																<li><a href="index.html">Home v1</a></li>
 																<li class="wt-newnoti"><a href="indexvtwo.html">Home v2<em>without login</em></a></li>
 															</ul>
 														</li>
-														<li class="menu-item-has-children page_item_has_children"><span class="wt-dropdowarrow"><i class="lnr lnr-chevron-right"></i></span><span class="wt-dropdowarrow"><i class="lnr lnr-chevron-right"></i></span>
+														<li class="menu-item-has-children page_item_has_children"><span class="wt-dropdowarrow"><i class="lnr lnr-chevron-right"></i></span>
 															<a href="javascript:void(0);">Article</a>
 															<ul class="sub-menu">
 																<li><a href="articlelist.html">Article List</a></li>
@@ -319,7 +331,7 @@
 																<li><a href="articleclassic.html">Article Classic</a></li>
 															</ul>
 														</li>
-														<li class="menu-item-has-children page_item_has_children"><span class="wt-dropdowarrow"><i class="lnr lnr-chevron-right"></i></span><span class="wt-dropdowarrow"><i class="lnr lnr-chevron-right"></i></span>
+														<li class="menu-item-has-children page_item_has_children"><span class="wt-dropdowarrow"><i class="lnr lnr-chevron-right"></i></span>
 															<a href="javascript:void(0);">Company</a>
 															<ul class="sub-menu">
 																<li><a href="companygrid.html">Company Grid</a></li>
@@ -343,7 +355,7 @@
 												<li class="nav-item">
 													<a href="howitworks.html">How It Works</a>
 												</li>
-												<li class="menu-item-has-children page_item_has_children"><span class="wt-dropdowarrow"><i class="lnr lnr-chevron-right"></i></span>
+												<li class="menu-item-has-children page_item_has_children">
 													<a href="javascript:void(0);">Browse Jobs</a>
 													<ul class="sub-menu">
 														<li>
@@ -357,7 +369,7 @@
 														</li>
 													</ul>
 												</li>
-												<li class="menu-item-has-children page_item_has_children"><span class="wt-dropdowarrow"><i class="lnr lnr-chevron-right"></i></span>
+												<li class="menu-item-has-children page_item_has_children">
 													<a href="javascript:void(0);">View Freelancers</a>
 													<ul class="sub-menu">
 														<li>
@@ -520,5 +532,6 @@
 					</div>
 				</div>
 			</header>
-			<!-- Header END-->
-			@endif
+			<!--Header End-->
+	
+@endif	
